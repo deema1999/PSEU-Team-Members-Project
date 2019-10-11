c98903a1f657849bb7a294fc5f7f795840c4d841
Deema Hafiz <hafezdema1999@gmail.com>
2019-10-11 22:52:51 +0300

    Add function to show the members with thier information

--- index.js
+++ index.js
@@ -28,6 +28,57 @@ function saveFunction() {
     localStorage.setItem('members-array', jsonMembers)
 } 
 
+function showMemberList(){
+    members.forEach(function(m){ 
+    let sec = document.createElement('div');
+    sec.style.marginLeft = "20px";
+    sec.style.marginTop = "30px";
+    
+    let icon = document.createElement('img');
+    icon.style.width = "40px";
+    icon.style.hight = "40px";
+    icon.style.float = "left";
+    icon.style.marginTop = "20px";
+    icon.style.marginRight = "5px";
+    icon.src = "icon1.PNG";
+    sec.appendChild(icon);
+    
+    /*let deleteBtn = document.createElement('button');
+    deleteBtn.style.backgroundColor = "#FF4A4A";
+    deleteBtn.style.width = "40px";
+    deleteBtn.style.height = "40px";
+    deleteBtn.style.float = "left";
+    deleteBtn.style.marginTop = "20px";
+    deleteBtn.style.marginRight = "10px";
+    deleteBtn.style.borderRadius = "50%";
+    sec.appendChild(deleteBtn);*/
+
+    let nameH = document.createElement('H2'); 
+    let list = document.getElementById('list'); 
+    nameH.appendChild(document.createTextNode(m.name)); 
+    sec.appendChild(nameH);
+    
+
+    let p = document.createElement('p');
+    p.style.color = "#2D89E6";
+    p.appendChild(document.createTextNode(m.email+" / "+m.major+" / "+m.role));
+    sec.appendChild(p);
+    
+
+    let bioP = document.createElement('p');
+    bioP.style.color = "#6A6A6A";
+    bioP.style.marginLeft = "43px";
+    bioP.style.height = "40px";
+    bioP.style.lineHeight ="20px";
+    bioP.style.overflow = "hidden";
+    bioP.appendChild(document.createTextNode(m.bio));
+    sec.appendChild(bioP);
+    
+    list.appendChild(sec);
+  })
+}
+
+
  
 
 
