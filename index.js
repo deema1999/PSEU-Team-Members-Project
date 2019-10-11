915cfad5f7991b6ebfdd5008423e599a54ca2b40
Deema Hafiz <hafezdema1999@gmail.com>
2019-10-11 22:24:21 +0300

    Create js file with add member functionality

---
+++ index.js
@@ -0,0 +1,33 @@
+let members=[];  
+let chechBtn;
+
+function member(name,email,major,role,bio) {
+    this.name = name;
+    this.email = email;
+    this.major = major;
+    this.role = role;
+    this.bio = bio;
+ }
+
+
+ function addMember(){
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let major = document.getElementById("major-list");
+    let stdMajor = major.options[major.selectedIndex].text;
+    let role = document.getElementById("role-list");
+    let stdRole = role.options[role.selectedIndex].text;
+    let bio = document.getElementById('bio').value;
+
+    let student = new member(name,email,stdMajor,stdRole,bio);
+
+    members.unshift(student);
+    
+ }
+ 
+
+ 
+
+
+ 
+ 
